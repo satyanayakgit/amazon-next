1fe66b57508718fe7cbf0a45f174cc6af26b001d
satyanayakgit <satyanayak2040@gmail.com>
2023-08-29 05:59:09 +0530

    header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -6,7 +6,7 @@ const Header = () => {
     return <div className="w-full h-20 bg-amazon_blue text-lightText sticky top-0 z-50">
         <div className="h-full w-full mx-auto inline-flex items-center justify-between gap-1 mdl:gap-3 px-4">
              {/* logo */}
-             <div className="px-2 border border-transparent hover:border-white cursor-pointer duration-300 flex items-center justify-center">
+             <div className="px-2 border border-transparent hover:border-white cursor-pointer duration-300 flex items-center justify-center h-[70%]">
                 <Image className="w-28 object-cover" src={logo} alt="logoImg" />
              </div>
              
