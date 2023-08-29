99c4a79c0eb70cb1508079c15cc37af9c7714866
satyanayakgit <satyanayak2040@gmail.com>
2023-08-29 05:57:36 +0530

    header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -5,7 +5,16 @@ import logo from "../../images/logo.png"
 const Header = () => { 
     return <div className="w-full h-20 bg-amazon_blue text-lightText sticky top-0 z-50">
         <div className="h-full w-full mx-auto inline-flex items-center justify-between gap-1 mdl:gap-3 px-4">
-             <Image className="w-28 object-cover" src={logo} alt="logoImg" />
+             {/* logo */}
+             <div className="px-2 border border-transparent hover:border-white cursor-pointer duration-300 flex items-center justify-center">
+                <Image className="w-28 object-cover" src={logo} alt="logoImg" />
+             </div>
+             
+             {/* delivery */}
+             {/* search bar */}
+             {/* signin */}
+             {/* favourite */}
+             {/* cart */}
         </div>
     </div>;
 };
