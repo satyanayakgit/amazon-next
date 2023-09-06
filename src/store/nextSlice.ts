7e30f76e23bd019deef17a649c85faee475c01d1
satyanayakgit <satyanayak2040@gmail.com>
2023-09-06 14:01:01 +0530

    redux

--- src/store/nextSlice.ts
+++ src/store/nextSlice.ts
@@ -1,6 +1,14 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { StoreProduct } from "../../type";
 
-const initialState = {
+interface NextState {
+  productData: StoreProduct[];
+  favoriteData: StoreProduct[];
+  allProducts: StoreProduct[];
+  userInfo: null | string;
+}
+
+const initialState: NextState = {
   productData: [],
   favoriteData: [],
   allProducts: [],
@@ -12,7 +20,48 @@ export const nextSlice = createSlice({
   initialState,
   reducers: {
     addToCart: (state, action) => {
-      state.productData = action.payload;
+      const existingProduct = state.productData.find(
+        (item: StoreProduct) => item._id === action.payload._id
+      );
+      if (existingProduct) {
+        existingProduct.quantity += action.payload.quantity;
+      } else {
+        state.productData.push(action.payload);
+      }
+    },
+    addToFavorite: (state, action) => {
+      const existingProduct = state.favoriteData.find(
+        (item: StoreProduct) => item._id === action.payload._id
+      );
+      if (existingProduct) {
+        existingProduct.quantity += action.payload.quantity;
+      } else {
+        state.favoriteData.push(action.payload);
+      }
+    },
+    increaseQuantity: (state, action) => {
+      const existingProduct = state.favoriteData.find(
+        (item: StoreProduct) => item._id === action.payload._id
+      );
+      existingProduct && existingProduct.quantity++;
+    },
+    decreaseQuantity: (state, action) => {
+      const existingProduct = state.favoriteData.find(
+        (item: StoreProduct) => item._id === action.payload._id
+      );
+      if (existingProduct?.quantity === 1) {
+        existingProduct.quantity = 1;
+      } else {
+        existingProduct && existingProduct.quantity--;
+      }
+    },
+    deleteProduct: (state, action) => {
+      state.productData = state.productData.filter(
+        (item) => item._id !== action.payload._id
+      );
+    },
+    resetCart: (state) => {
+      state.productData = [];
     },
   },
 });
